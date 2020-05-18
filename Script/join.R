4f07f2c2c06007c78c865619ff873e93d5deadef
gylfii <gylfii@users.noreply.github.com>
2020-05-18 13:51:13 +0000

    Script frá Gunnari til að finna réttu svörinn

---
+++ Script/join.R
@@ -0,0 +1,33 @@
+library(Matrix)
+library(dbplyr)
+library(dplyr)
+library(RMySQL)
+Sys.setlocale("LC_ALL", "is_IS.UTF-8")
+library(RMySQL)
+library(MASS)
+
+
+
+twDb<-src_mysql("tw_quizdb", host = "127.0.0.1", port = 3306, user = "gss24", password = "JuwofKWT2Ewc")
+question <- tbl(twDb,"question")
+student<-tbl(twDb,"student")
+answer<-tbl(twDb,"answer")
+lecture<-tbl(twDb,"lecture")
+host<-tbl(twDb,"host")
+coinAward<-tbl(twDb,"coinAward")
+lectureStudentSetting<-tbl(twDb,"lectureStudentSetting")
+lectureStudentSetting%>%group_by(key)%>%summarise(stdev=sd(value,na.rm=T),stdevlog=sd(log2(value),na.rm=T))->stdevs
+hashes<-read.csv("Data/bighashfile.txt",sep=" ",col.names = c("dir","qName","hash","numQ","notaType"))
+pathQ<-unique(paste(hashes$dir,hashes$qName,sep=""))
+question%>%filter(plonePath%in%pathQ)->myQuestions
+answer%>%filter(timeStart>"2020-01-01 00:01:01")->answerRed
+inner_join(answerRed,myQuestions) -> myAnswer
+
+#
+# myAnswer should now contain all answers to just the questions listed in the hash file
+#
+
+glimpse(myAnswer)
+myAnswer %>% summarise(n())
+
+MyAnswer <- as.data.frame(myAnswer)
