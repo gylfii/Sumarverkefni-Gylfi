4c42f233e106f315e8a56e49ea21f84ef3c414ee
gylfii <gylfii@users.noreply.github.com>
2020-05-22 11:13:21 +0000

    Bætt við fleiri töflur til að skoða
    Lesið yfir þær töflur og prufað að teikna smá

--- Script/Byrjunarvinnsla.R
+++ Script/Byrjunarvinnsla.R
@@ -4,9 +4,9 @@ library(RMySQL)
 library(tidyr)
 library(MASS)
 library(ggplot2)
-library(hash)
+library(gridExtra)
 
-#lesum inn gögninn okkar
+#lesum inn gögninn okkar, fyrst fyrir NOTA+ skoðað eins og hitt
 hashAnswer <- read.csv('Data/hashAnswer.csv')
 hashAnswer <- hashAnswer %>% subset(select=-c(X))
 hashAnswer$hsta <- hashAnswer$hsta%>%as.character()
@@ -15,20 +15,106 @@ ans.glm <- glm(correct~fsfat,family = binomial(link="logit"),data=hashAnswer)
 ans.glm2 <- glm(correct~fsfat+hsta,family = binomial(link="logit"),data=hashAnswer)
 ans.glm3 <- glm(correct~fsfat*hsta,family = binomial(link="logit"),data=hashAnswer)
 
+#næst fyrir þegar NOTA+ er horft á aðeins öðruvísi
+hashAnswer2 <- read.csv('Data/hashAnswer2.csv')
+hashAnswer2 <- hashAnswer2 %>% subset(select=-c(X))
+hashAnswer2$hsta <- hashAnswer2$hsta%>%as.character()
+
+ans2.glm <- glm(correct~fsfat,family = binomial(link="logit"),data=hashAnswer2)
+ans2.glm2 <- glm(correct~fsfat+hsta,family = binomial(link="logit"),data=hashAnswer2)
+ans2.glm3 <- glm(correct~fsfat*hsta,family = binomial(link="logit"),data=hashAnswer2)
+
+#næst er að skoða fyrir þegar NOTA + er horft á miðað við fyrst skiptið í hverjum fyrirlestri
+hashAnswer3 <- read.csv('Data/hashAnswer3.csv')
+hashAnswer3 <- hashAnswer3 %>% subset(select=-c(X))
+hashAnswer3$hsta <- hashAnswer3$hsta%>%as.character()
+
+ans3.glm <- glm(correct~fsfat,family = binomial(link="logit"),data=hashAnswer3)
+ans3.glm2 <- glm(correct~fsfat+hsta,family = binomial(link="logit"),data=hashAnswer3)
+ans3.glm3 <- glm(correct~fsfat*hsta,family = binomial(link="logit"),data=hashAnswer3)
+
+#Svo er gögninn öll, án NOTA+
+hashAnswer4 <- read.csv('Data/hashAnswer4.csv')
+hashAnswer4 <- hashAnswer4 %>% subset(select=-c(X))
+hashAnswer4$hsta <- hashAnswer4$hsta%>%as.character()
+
+ans4.glm <- glm(correct~fsfat,family = binomial(link="logit"),data=hashAnswer4)
+ans4.glm2 <- glm(correct~fsfat+hsta,family = binomial(link="logit"),data=hashAnswer4)
+ans4.glm3 <- glm(correct~fsfat*hsta,family = binomial(link="logit"),data=hashAnswer4)
 
 ?glm
 
 #Teiknum fyrst aðeins gögninn með 
-ggplot(data=cbind(hashAnswer,pred=predict.glm(ans.glm,type = "response")),aes(x=fsfat,y=correct))+
-  geom_point()+
-  geom_line(aes(y=pred))
-ggplot(data=cbind(hashAnswer,pred=predict.glm(ans.glm2,type = "response")),aes(x=fsfat,y=correct,color=hsta))+
-  geom_point()+
-  geom_line(aes(y=pred))
-ggplot(data=cbind(hashAnswer,pred=predict.glm(ans.glm3,type = "response")),aes(x=fsfat,y=correct, color=hsta))+
+p11 <- ggplot(data=cbind(hashAnswer,pred=predict.glm(ans.glm,type = "response")),aes(x=fsfat,y=correct))+
   geom_point()+
   geom_line(aes(y=pred))+
-  xlim(0,80)
+  ggtitle("Answer1")
+p12 <- ggplot(data=cbind(hashAnswer,pred=predict.glm(ans.glm2,type = "response")),aes(x=fsfat,y=correct,color=hsta))+
+  geom_point()+
+  geom_line(aes(y=pred))+
+  ggtitle("Answer1")
+p13 <- ggplot(data=cbind(hashAnswer,pred=predict.glm(ans.glm3,type = "response")),aes(x=fsfat,y=correct, color=hsta))+
+  geom_point()+
+  geom_line(aes(y=pred))+
+  ggtitle("Answer1")
+
+p21 <- ggplot(data=cbind(hashAnswer2,pred=predict.glm(ans2.glm,type = "response")),aes(x=fsfat,y=correct))+
+  geom_point()+
+  geom_line(aes(y=pred))+
+  ggtitle("Answer2")
+p22 <- ggplot(data=cbind(hashAnswer2,pred=predict.glm(ans2.glm2,type = "response")),aes(x=fsfat,y=correct,color=hsta))+
+  geom_point()+
+  geom_line(aes(y=pred))+
+  ggtitle("Answer2")
+p23 <- ggplot(data=cbind(hashAnswer2,pred=predict.glm(ans2.glm3,type = "response")),aes(x=fsfat,y=correct, color=hsta))+
+  geom_point()+
+  geom_line(aes(y=pred))+
+  ggtitle("Answer2")
+
+p31 <- ggplot(data=cbind(hashAnswer3,pred=predict.glm(ans3.glm,type = "response")),aes(x=fsfat,y=correct))+
+  geom_point()+
+  geom_line(aes(y=pred))+
+  ggtitle("Answer3")
+p32 <- ggplot(data=cbind(hashAnswer3,pred=predict.glm(ans3.glm2,type = "response")),aes(x=fsfat,y=correct,color=hsta))+
+  geom_point()+
+  geom_line(aes(y=pred))+
+  ggtitle("Answer3")
+p33 <- ggplot(data=cbind(hashAnswer3,pred=predict.glm(ans3.glm3,type = "response")),aes(x=fsfat,y=correct, color=hsta))+
+  geom_point()+
+  geom_line(aes(y=pred))+
+  ggtitle("Answer3")
+
+p41 <- ggplot(data=cbind(hashAnswer4,pred=predict.glm(ans4.glm,type = "response")),aes(x=fsfat,y=correct))+
+  geom_point()+
+  geom_line(aes(y=pred))+
+  ggtitle("Answer4")
+p42 <- ggplot(data=cbind(hashAnswer4,pred=predict.glm(ans4.glm2,type = "response")),aes(x=fsfat,y=correct,color=hsta))+
+  geom_point()+
+  geom_line(aes(y=pred))+
+  ggtitle("Answer4")
+p43 <- ggplot(data=cbind(hashAnswer4,pred=predict.glm(ans4.glm3,type = "response")),aes(x=fsfat,y=correct, color=hsta))+
+  geom_point()+
+  geom_line(aes(y=pred))+
+  ggtitle("Answer4")
+
+grid.arrange(p11, p21, nrow=1)
+grid.arrange(p12, p22, nrow=1)
+grid.arrange(p13, p23, nrow=1)
+
+grid.arrange(p11, p21, p31, nrow=1)
+grid.arrange(p12, p22, p32, nrow=1)
+grid.arrange(p13, p23, p33, nrow=1)
+
+grid.arrange(p11, p21, p31, p41, nrow=2)
+grid.arrange(p12, p22, p32, p42, nrow=2)
+grid.arrange(p13, p23, p33, p43, nrow=2)
+
+hashAnswer %>% summarise(n_distinct(studentId))
+hashAnswer %>% filter(fsfat>300) %>% summarise(n_distinct(studentId))
+
+hashAnswer %>% group_by(lectureId) %>% summarise(n_distinct())
+
+ggplot(data=hashAnswer)+geom_bar(aes(x=hsta,y=stat(prop),group=1))
 
 
 summary(ans.glm)
--- Script/answertoflugerd.R
+++ Script/answertoflugerd.R
@@ -4,7 +4,6 @@ library(RMySQL)
 library(tidyr)
 library(MASS)
 library(ggplot2)
-library(hash)
 #les inn öll gögninn, nota mestalagi bara answer gagnasafnið
 twDb<-src_mysql("tw_quizdb", host = "127.0.0.1", port = 3306, user = "gss24", password = "JuwofKWT2Ewc")
 answer <- tbl(twDb, "answer")
@@ -24,7 +23,24 @@ MyAnswer <- MyAnswer[order(MyAnswer$timeStart),] %>% group_by(lectureId,studentI
 hashAnswer <- inner_join(MyAnswer,hashes)
 minstadagsetning <- hashAnswer %>% group_by(studentId,hash) %>% summarise('mindag'=min(timeStart))
 hashAnsdag <- full_join(hashAnswer,minstadagsetning)
+
+hashAnsdag2 <- hashAnsdag
+hashAnsdag2$hsta <- ifelse(hashAnsdag2$mindag==hashAnsdag2$timeStart | hashAnsdag2$hash=='NOTA+',0,1)
+
+hashAnsdag3 <- hashAnsdag %>% group_by(lectureId,studentId,hash) %>% summarise('minNotadag'=min(timeStart)) %>%
+  full_join(hashAnsdag)
+hashAnsdag3$hsta <- ifelse(hashAnsdag3$mindag==hashAnsdag3$timeStart |
+                             (hashAnsdag3$hash=='NOTA+' & hashAnsdag3$minNotadag==hashAnsdag3$timeStart),0,1)
+
 hashAnsdag$hsta <- ifelse(hashAnsdag$mindag==hashAnsdag$timeStart,0,1)
+hashAnsdag4 <- hashAnsdag[!grepl('NOTA+',hashAnsdag$hash),]
+
 #Vel dálka og save-a
-hashanswers <- hashAnsdag %>% dplyr::select(lectureId,studentId,questionId,correct,fsfat,hsta)
+hashanswers <- hashAnsdag %>% dplyr::select(lectureId,studentId,questionId,correct,hash,fsfat,hsta)
+hashanswers2 <- hashAnsdag2 %>% dplyr::select(lectureId,studentId,questionId,correct,hash,fsfat,hsta)
+hashanswers3 <- hashAnsdag3 %>% dplyr::select(lectureId,studentId,questionId,correct,hash,fsfat,hsta)
+hashanswers4 <- hashAnsdag4 %>% dplyr::select(lectureId,studentId,questionId,correct,hash,fsfat,hsta)
 write.csv(hashanswers,'Data/hashAnswer.csv')
+write.csv(hashanswers2,'data/hashAnswer2.csv')
+write.csv(hashanswers3,'Data/hashAnswer3.csv')
+write.csv(hashanswers4,'Data/hashAnswer4.csv')
