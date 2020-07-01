c6968b42754347f7a5f5ab20861b31b5958e352d
gylfii <gylfii@users.noreply.github.com>
2020-07-01 11:43:36 +0000

    set er upp grunninn fyrir lokaskýrsluna og þannig að hægt sé að save-a það sem pdf.
    Former-commit-id: 9aafb78176fa8c76e70431dcbd9a245aa939f25d

--- Script/Settings.R
+++ Script/Settings.R
@@ -13,3 +13,16 @@ library(latex2exp)
 library(knitr)
 library(cvAUC)
 library(car)
+library(ggthemes)
+library(ggpubr)
+library(kableExtra)
+library(bookdown)
+library(scales)
+library(tinytex)
+theme_set(theme_tufte() +
+            theme(panel.border = element_rect('black', fill = NA),
+                  text = element_text(size = 14),
+                  legend.text=element_text(size=14),
+                  axis.text=element_text(size=14),
+                  axis.title = element_text(size = 14),
+                  plot.title = element_text(hjust = 0.5)))
